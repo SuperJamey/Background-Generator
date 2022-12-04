25651faa205c664aeb1017455cdc4886bc302d9d
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2022-12-04 16:25:11 -0600

    Add click event for button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector(".button");
 
 setGradient(); /*Display initial linear gradient properties on load*/
 
@@ -16,5 +17,9 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function buttonCLick() {
+    console.log("cutton clicked");
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+button.addEventListener("click", buttonClick);
