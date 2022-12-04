cb1236c9263cc5b42899cd20b2c08532cd56d2da
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2022-12-03 20:50:52 -0600

    Add initial gradient properties on load

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+setGradient(); /*Display initial linear gradient properties on load*/
+
 function setGradient() {
     body.style.background =
     "linear-gradient(to right, "
